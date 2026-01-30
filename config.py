9b26789a487918748bbd2d23b612e1aaddaf6dcf
Dotachin <siddharthamanu5@gmail.com>
2026-01-30 12:11:02 +0530

    Base skeletal of Shortner Created

---
+++ config.py
@@ -0,0 +1,25 @@
+#URLshortner_app/config.py
+
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    env_name : str = "Local"
+    base_url : str = "http://localhost:8000"
+    db_url : str = "sqlite:///./shortner.db"
+
+    class config:
+        env_file = ".env"
+
+
+def get_settings() -> Settings:
+    settings = Settings()
+    print(f"Loading settings for {settings.env_name}")
+    return settings
+
+@lru_cache
+def get_settings() -> Settings:
+    settings = Settings()
+    print(f"Loading settings for {settings.env_name}")
+    return settings
---
+++ crud.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+import keygen, models, schemas
+
+def check_unique_key(db : Session, url_key : str) -> models.URL:
+    return(db.query(models.URL).filter(models.URL.key == url_key).first())
+
+
+
+def create_db_url(db : Session, url : schemas.URLBase) -> models.URL:
+    key = keygen.create_unique_random_key(db)
+    secret_key = f"{key}_{keygen.create_random_key(8)}"
+    db_url = models.URL(target_url = url.target_url, key = key, secret_key = secret_key)
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    return db_url
